0df19998f3dc198ccf92a20c99dd6e7bf74570ae
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-06 03:52:30 +0530

    fix: fixed opacity not working on colors

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/ui/Button";
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between bg-background p-24">
-            <Button variant={"primary"}>Button</Button>
+            <Button variant={"primary"} className="bg-primary">Button</Button>
         </main>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
         extend: {
             colors: {
-                primary: "var(--color-primary)",
-                secondary: "var(--color-secondary)",
-                accent: "var(--color-accent)",
-                text: "var(--color-text)",
-                background: "var(--color-background)",
-            }
-        }
+                primary: "rgb(var(--color-primary) / <alpha-value>)",
+                secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+                accent: "rgb(var(--color-accent) / <alpha-value>)",
+                text: "rgb(var(--color-text) / <alpha-value>)",
+                background: "rgb(var(--color-background) / <alpha-value>)",
+            },
+        },
     },
-    plugins: [require('@tailwindcss/forms')],
-}
+    plugins: [require("@tailwindcss/forms")],
+};
