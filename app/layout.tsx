a5e8a47ca8a7055ffdec1ccd4799f7ca8a4aff41
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-12-29 21:44:35 +0530

    udpated packges to their latest version and migrated nextjs to version 14

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from 'react-hot-toast'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-    title: 'Nextjs 13, TailwindCss Template',
+    title: 'Nextjs 14, TailwindCss Template',
     description:
         'A template for frontend developers created By Chirag Bhalotia',
 }
