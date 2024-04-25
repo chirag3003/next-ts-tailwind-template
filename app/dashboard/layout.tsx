091a2b923ce4c1fbde99b227e66973f7954e60ee
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-04-26 02:59:22 +0530

    updated

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,9 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react'
 
-import Navbar from "../../components/ui/Navbar";
+import Navbar from '@/components/Navbar'
 
 interface DashboardLayoutProps {
-    children: React.ReactNode;
+    children: React.ReactNode
 }
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
@@ -12,9 +12,11 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
             <div className="min-h-full">
                 <Navbar />
                 <main className="pt-10">
-                    <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">{children}</div>
+                    <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+                        {children}
+                    </div>
                 </main>
             </div>
         </>
-    );
+    )
 }
